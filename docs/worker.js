0f672f8e9935739940f86fef00afb3eb0d02f3b3
Morteza Jamshidi <devmor.j@gmail.com>
2022-06-28 18:31:18 +0430

    build docs folder for github pages

---
+++ docs/worker.js
@@ -0,0 +1,18 @@
+// self is like global object window
+// but here refers to this web worker
+
+// listen on messages sent here
+// comming from it's registerer - in this case index.js
+
+self.onmessage = ({ data }) => {
+  const isDataInvalid = data.some(
+    d => d === '' || typeof d === NaN
+  )
+
+  if (isDataInvalid) {
+    self.postMessage('data is invalid (all fields must be numbers)')
+  } else {
+    const result = data.reduce((prev, curr) => prev * curr)
+    self.postMessage(result)
+  }
+}
