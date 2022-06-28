d401f8c9cf39eece83a37a8464af34e6cc94557b
Morteza Jamshidi <devmor.j@gmail.com>
2022-06-28 18:03:15 +0430

    create a web worker to multiply number inputs

---
+++ index.js
@@ -0,0 +1,22 @@
+const resultElement = document.getElementById('result')
+
+const numberInputs = [...document.querySelectorAll('[type="number"][data-number]')]
+
+// check support for web workers
+
+if ('Worker' in window) {
+  const worker = new Worker('./worker.js');
+
+  numberInputs.forEach(el => {
+    el.addEventListener('input', () => {
+      const data = numberInputs.map(el => el.value)
+      worker.postMessage(data)
+    })
+  })
+
+  worker.onmessage = ({ data }) => {
+    resultElement.textContent = data
+  }
+} else {
+  resultElement.textContent = 'Sorry! your browser does not support web workers, maybe because it is outdated!'
+}
